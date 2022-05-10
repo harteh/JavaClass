87d83d38aa4d4d9e6234969e06793d071c7e8206
harteh <harteh31@gmail.com>
2022-05-10 15:33:06 +0900

    modify

--- bm_workspace/BaekjoonJava/src/MainT01.java
+++ bm_workspace/BaekjoonJava/src/MainT01.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class MainT01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("현재 시간(시각, 분)을 입력하세요 : ");
+		int CurrentA = sc.nextInt();	//현재 시각 (0 ≤ A ≤ 23)
+		int CurrentB = sc.nextInt();	//현재 분 (0 ≤ B ≤ 59)
+		
 
 	}
 
