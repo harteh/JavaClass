a474322fa8354bebaf01f8c6418fba17298e70ae
harteh <harteh31@gmail.com>
2022-05-12 11:25:20 +0900

    array 02

--- bm_workspace/BaekjoonJava/src/MainT01.java
+++ bm_workspace/BaekjoonJava/src/MainT01.java
@@ -8,25 +8,17 @@ public class MainT01 {
 		
 		//오븐시계
 		System.out.println("현재 시간(시각, 분)을 입력하세요 : ");
-		int cA = sc.nextInt();	//현재 시각 (0 ≤ A ≤ 23)
-		int cB = sc.nextInt();	//현재 분 (0 ≤ B ≤ 59)
+		int A = sc.nextInt();	//현재 시각 (0 ≤ A ≤ 23)
+		int B = sc.nextInt();	//현재 분 (0 ≤ B ≤ 59)
 
 		System.out.println("요리하는데 걸리는 시간을 분단위로 입력하세요 : ");
-		int uC = sc.nextInt();		//요리하는데 걸리는 시간 분(0 ≤ C ≤ 1,000)
+		int C = sc.nextInt();		//요리하는데 걸리는 시간 분(0 ≤ C ≤ 1,000)
 		
-		
-//		int endA;	//종료 시각
-		int endB;	//종료 분
-		
-		//endB 가 59이상이 되면 endA가 +1 된다
-		
-		endB = cB + uC;
-		
-		if (endB > 59) {
-			cA += 1;
-			endB -= 60;
+		if (B + C > 59) {
+			A += 1;
+			B -= 60;
 		}
-		System.out.printf("종료시간은 %d : %d", cA,endB);
+		System.out.printf("종료시간은 %d : %d", A,B);
 		
 
 		
--- bm_workspace/MayJava/src/TestArray01.java
+++ bm_workspace/MayJava/src/TestArray01.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 배열(array)
  * - 한번에 많은 메모리 공간 선언 가능
@@ -6,9 +8,20 @@
  * 
  * int 		ary[]	= new int[10]
  * 자료형	배열명[]= new 자료형[데이터개수];
- * '배열 선언' 		= '배열 생성'
+ * -'배열 선언' 		= '배열 생성'
+ * - int형 데이터가 10개인 배열 : 40바이트
  * 
- *  - int형 데이터가 10개인 배열 : 40바이트
+ * 배열선언
+ * - int a[];	//배열에 대한 레퍼런스 변수 a를 선언
+ * 
+ * 배열 초기화
+ *  int ary[] = {1,2,3};
+ *  - 초기화할때는 []개수 적으면 안됨.
+ *  - 입력 순서대로 저장된다.
+ *  
+ *  배열 출력
+ *  System.out.println(ary[0]);
+ *  
  *
  */
 
@@ -18,6 +31,58 @@ public class TestArray01 {
 		
 		//1. 정수 5개를 저장할 배열
 		int arr1[] = new int[5];
+		//실수 10개 저장할 배열
+		double arr2[] = new double[10];
+		//배열 요소 수가 세개인 int형 배열
+		int arr3[] = new int[3];
+		//인덱스가 최대값이 4인 char형 배열
+		char arr4[] = new char[5];
+		
+		//for문으로 배열 출력하기
+		int a[] = {1,2,3,4,5,6,7,8,9,10};
+		for (int i = 0; i < 10; i++) {
+			System.out.print(a[i]);
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("****** 입력받은 값을 배열에 저장,출력 ******");
+//		double arr5[] = new double[3];	//실수형 3개 배열 생성
+//		
+//		for (int i = 0; i < 3; i++) {
+//			arr5[i] = sc.nextDouble();	//실수 3번를 입력받아 arr5[i]에 저장한다.
+//			System.out.println(arr5[i]);
+//		}
+		
+
+		System.out.println("****** 입력받은 값의 합 ******");
+//		double arr6[] = new double[5];
+//		int sum = 0;
+//		
+//		for (int i = 0; i < 5; i++) {
+//			arr6[i]=sc.nextDouble();
+//			sum += arr6[i];
+//			//실수를 입력받아 배열에 저장
+//		}
+//		System.out.println(sum/5.0);
+		
+
+		System.out.println("****** 큰 값 출력 ******");
+		int arr7[] = new int[5];
+		int max = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			arr7[i] = sc.nextInt();
+			if (arr7[i] > max) {	//입력받은 값이 최대값(max)보다 크면,
+				max = arr7[i];		//입력값을 max에 저장
+			}
+		}
+		System.out.println("가장 큰 수는 "+max);
+		
+
+		System.out.println("******  ******");
+		int arr8[] = {10,20,30,40,50};
 		
 		
 
