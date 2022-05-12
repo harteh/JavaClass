15033c6c432844cd9cd1c7af050ebc4cd4b37eee
harteh <harteh31@gmail.com>
2022-05-12 13:19:29 +0900

    array 04

---
+++ bm_workspace/BaekjoonJava/src/Main10818.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ * 
+ * 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 
+ * 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 
+ * 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+ * 
+ * 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+ *
+ */
+public class Main10818 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//1. 배열의 크기 n 을 입력받는다.
+		int n = sc.nextInt();
+		//2. n크기의 배열을 생성한다.
+		int ary[] = new int[n];
+		
+		//3. n개의 값을 입력받아 배열에 저장한다.
+		for (int i=0; i<n-1; i++) {
+			ary[i] = sc.nextInt();
+			System.out.println( ary[i] );
+		}
+		
+		
+	}
+
+}
--- bm_workspace/MayJava/src/Array02.java
+++ bm_workspace/MayJava/src/Array02.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 배열 api
  * https://docs.oracle.com/javase/8/docs/api/index.html
@@ -6,6 +8,15 @@
  * - 배열의 각 요소를 순차적으로 접근한다.
  * 
  * for(int i : num){}
+ * 
+ *   ** 2차원 배열 **  
+ * - 행렬
+ * int ary[][];
+ * ary = new int[2][5];
+ * -> int ary[][] = new int[2][5];
+ * -> 2행 5열
+ * [][][][][]
+ * [][][][][]
  *
  */
 public class Array02 {
@@ -25,12 +36,55 @@ public class Array02 {
 		System.out.println();
 		
 		String fruit[] = {"바나나", "딸기", "사과"};
-		for(String i : fruit) {
-			System.out.print(i);
+		for(String s : fruit) {
+			System.out.print(s);
 		}
 		System.out.println();
 		
-		System.out.println("******  ******");
+		
+		System.out.println("****** 2차원 배열 ******");
+		//하나의 문자 5행 5열 구조
+		char ary[][] = new char[5][5];
+		//실수형 5행 2열 구조
+		double ary1[][] = new double[5][2];
+
+		
+		System.out.println("****** 2차원 배열 초기화 ******");
+		int c[][] = {{1,2,3},
+					 {4,5,6}};
+		
+		for(int i=0; i<2; i++) {	//행
+			for(int j=0; j<3; j++) {	//열
+				System.out.println( c[i][j] );
+			}
+		}
+		
+
+		System.out.println("****** 실수형 2차원 2행 3열 구조 ******");
+		Scanner sc = new Scanner(System.in);
+		double ary2[][] = new double[2][3];
+		
+//		for (int i=0; i<2; i++) {
+//			for(int j=0; j<3; j++) {
+//				ary2[i][j]=sc.nextDouble();
+//				System.out.println( ary2[i][j]);
+//			}
+//		}
+
+
+		System.out.println("****** 정수 10개 입력받아 5의 배수 출력(1차원배열) ******");
+		int ary3[] = new int[10];
+		
+		for (int i=0; i<ary3.length; i++) {
+			ary3[i]=sc.nextInt();
+			if (ary3[i]%5==0) {
+				System.out.println("5의배수:" + ary3[i]);
+			}
+		}
+		
+
+		
+		
 		
 
 	}
