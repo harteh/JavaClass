803b89af456d166bba1f62a0b6ca7cbe35157923
harteh <harteh31@gmail.com>
2022-05-13 13:09:59 +0900

    review 0513 end

--- bm_workspace/MayJava/src/Review0513.java
+++ bm_workspace/MayJava/src/Review0513.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -119,10 +120,131 @@ public class Review0513 {
 			} else {
 				break;
 			}
-
 		}
+		
+
+		System.out.println();
+		System.out.println("****** 정수 10개를 입력받아 평균 출력 ******");
+		//(배열, length필드 사용)
+		
+//		int arr[] = new int[10];
+//		int sum=0;
+//		
+//		for (int j = 0; j < arr.length; j++) {	//정수 저장
+//			arr[j] = sc.nextInt();
+//			sum += arr[j];
+//		}
+//		System.out.println( (double)sum / arr.length );
+		//계산 횟수는 배열의 길이를 사용하면 되니까 따로 변수안만들어도 됨..
+		
+		
+
+		System.out.println("****** 정수 10개를 입력받아 7의 배수만 출력 ******");
+		// (배열)
+		
+//		int arr1[] = new int[10];
+//		
+//		for(int h = 0; h<arr1.length; h++) {
+//			arr1[h] = sc.nextInt();
+//		}
+//		for (int j : arr1) {
+//			if (j%7==0) {
+//				System.out.print(j +" ");
+//			}
+//		}
+		
+		/* 
+		Scanner s = new Scanner(System.in);
+		int []a=new int[10];
+		int sum=0;
+		
+		System.out.println("정수 10개 입력");
+		for(i=0;i<a.length;i++) {
+			a[i]=s.nextInt();
+			if(a[i]%7==0) {
+				System.out.println(a[i]);
+			}
+			//sum+=a[i];
+		}
+		//System.out.println((double)sum/a.length);
+		 * 강사님 코드
+		 */
+
+/*
+		double sum=0.0;
+		double ary[]={1.0, 1.5, 2.0, 2.5};
+		for( i=0; i<4 ;i++) 
+		sum+=ary[i];
+		System.out.println(sum);	//7.0
+*/
+
+		System.out.println("****** while문 ******");
+		double sum=0.0;
+		double ary[]={1.0, 1.5, 2.0, 2.5};
+		i = 0;
+		while(i<ary.length) {
+			sum += ary[i];
+			i++;
+		}
+		System.out.println("while문: "+sum);
+		
+		System.out.println("****** do-while문 ******");
+		sum=0.0;
+		i = 0;
+		do {
+			sum += ary[i];
+			i++;
+		} while (i<ary.length);
+		System.out.println("do-while문: "+sum);
+		
+		
+		System.out.println("****** for-each문 ******");
+		sum=0.0;
+		
+		//배열 데이터가 double형이기 때문에 double 변수를 생성
+		for(double j : ary) {
+//			sum += ary[(int)j];	-> for-each문은 인덱스가 필요없다!!
+			sum += j;
+		}
+		System.out.println("for-each문: "+sum);
+		
+
+		System.out.println("****** 3행 3열 배열을 0~8까지정수로 초기화 ******");
+		int arr [][] = {{0,1,2},
+				{3,4,5},
+				{6,7,8}};
+		
+
+		System.out.println("****** 2행 3열 배열을 a~f까지문자로 초기화 ******");
+		char c[][]= {{'a','b'},{'c','d'},{'e','f'}};
+		
+		
+		System.out.println("****** 2차원 배열과 합 ******");
+		/*
+		 * 3행 4열의정수형 배열을 생성하여 0~9 범위의정수를 랜덤하게 저장
+		 * 저장후 2차원 배열과 합을 출력
+		 * 실행결과) 5 4 1 5
+		 * 			 0 5 3 5
+		 *           7 8 1 4
+		 *           합은 48 
+		 */
+		
+		int arr2[][] = new int[3][4];
+		sum = 0;
+		for (int j = 0; j < arr2.length; j++) {
+			for (int j2 = 0; j2 < arr2[j].length; j2++) {
+				arr2[j][j2] = (int)(Math.random()*10);
+				System.out.print(arr2[j][j2]+" ");
+				sum+=arr2[j][j2];
+			}
+			System.out.println();
+		}
+		System.out.println("합은 "+(int)sum);
+		
 
 
+		
+		
 		
 
 	}
