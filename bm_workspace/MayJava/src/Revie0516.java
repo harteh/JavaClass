13dfba397169baf6793f8162d7988c71ed4abf18
harteh <harteh31@gmail.com>
2022-05-16 14:47:04 +0900

    p.123 diamond

--- bm_workspace/MayJava/src/Revie0516.java
+++ bm_workspace/MayJava/src/Revie0516.java
@@ -85,24 +85,24 @@ public class Revie0516 {
 
 		System.out.println("********* p.123 Q4 *********");
 		//반복문으로 * 삼각형 그리기
-		int line=4;
-		int space=(line/2)+1;
-		int star=1;
-		
-		for(int i=0; i<line; i++) {
-			for (int j = 0; j < space; j++) {	//공백
-				System.out.print(' ');
-			}
-			for(int j=0; j<star; j++) {	//별
-				System.out.print("*");
-			}
-			for (int j = 0; j < space; j++) {	//공백
-				System.out.print(' ');
-			}
-			space-=1;
-			star+=2;
-			System.out.println();
-		}
+//		int line=4;
+//		int space=(line/2)+1;
+//		int star=1;
+//		
+//		for(int i=0; i<line; i++) {
+//			for (int j = 0; j < space; j++) {	//공백
+//				System.out.print(' ');
+//			}
+//			for(int j=0; j<star; j++) {	//별
+//				System.out.print("*");
+//			}
+//			for (int j = 0; j < space; j++) {	//공백
+//				System.out.print(' ');
+//			}
+//			space-=1;
+//			star+=2;
+//			System.out.println();
+//		}
 
 		System.out.println("********* p.123 Q4 *********");
 		for(int i=0; i<5; i++) {
@@ -114,11 +114,33 @@ public class Revie0516 {
 			}
 			System.out.println();
 		}
-		
+	
 
 		System.out.println("********* p.123 Q5 *********");
+		//7줄 마름모꼴 그리기
+
 		
-		
+		for (int i = 0; i <7; i++) {	//전체 라인
+		    if (i<4) {	//별 증가, 공백 감소 //행i: 0,1,2,3,4
+
+		        for (int j=0; j<3-i; j++) {//공백 4,3,2,1,0
+		            System.out.print(" ");					
+		        }
+		        for (int j=0; j<(i*2)+1; j++) {//별 1,2,3,4,5
+		            System.out.print("*");
+		        }
+
+		    } else {	//별 감소, 공백 증가	//행i: 5,6,7,8
+
+		        for (int j=0; j<i-3; j++) {		//공백 1,2,3,4
+		            System.out.print(" ");
+		        }
+		        for (int j = (7-i)*2; j >1 ; j--) {		//별 4,3,2,1
+		            System.out.print("*");
+		        }
+		    }
+		    System.out.println();
+		}
 		
 		
 		
