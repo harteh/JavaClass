402a2ed151b2629a88bddd292794a7a13c199b2d
harteh <harteh31@gmail.com>
2022-05-26 13:01:22 +0900

    boxing, unboxing

--- bm_workspace/MayJava/src/object/ObjEx01.java
+++ bm_workspace/MayJava/src/object/ObjEx01.java
@@ -1,5 +1,7 @@
 package object;
 
+/*
+ * 1 toString()
 
 class In {
 	private int num;
@@ -12,12 +14,99 @@ class In {
 	}
 }
 
-
-
 public class ObjEx01 {
 	public static void main(String[] args) {
 		In i = new In(4);
 		System.out.println(i);
+	}
+}
+*/
+
+/*
+ * 2 toString()
+
+class Po {
+	private int a, b;
+	Po(int a, int b){
+		this.a = a;		this.b = b;
+	}
+	
+	public String toString() {
+		return a+" "+b;
+	}
+}
+
+public class ObjEx01 {
+	public static void main(String[] args) {
 		
+		Po p = new Po(3,4);
+		System.out.println(p.getClass().getName());	//객체의 클래스 이름 출력
+		System.out.println(p.hashCode());		//해시코드의 16진수 값
+		System.out.println(p.toString());		//클래스명+주소값
+		
+		System.out.println(p.toString());	//toString() 재정의하면 3, 4가 출력됨
+	}
+}
+*/
+
+/*
+ * 3 .equals()
+
+class Po {
+	private int a, b;
+	Po(int a, int b){
+		this.a = a;		this.b = b;
+	}
+	
+	public boolean equals(Po p) {
+		if(this.a==p.a && this.b==p.b) {	
+			//자기자신의 a와 매개변수로 전달받은 값 비교
+			return true;
+		}
+		else
+			return false;
+	}
+	
+}
+
+public class ObjEx01 {
+	public static void main(String[] args) {
+		
+		Po p = new Po(3,4);
+		Po p1 = new Po(2,3);
+		Po p2 = new Po(3,4);
+		
+		if(p==p1) {
+			System.out.println("p==p1");	//주소값이 다르다
+		}
+		if(p.equals(p1)) {		
+			// 객체 p가 자기자신..(-> line 64), p1이 매개변수
+			System.out.println("p는 p1과 같다");			
+		}
+		if(p.equals(p2)) {
+			System.out.println("p는 p2와 같다");			
+		}
+	}
+}
+*/
+
+/*
+ * 4. p.386 Q4
+ */
+
+class MyDog{
+	String a, b;
+	MyDog(String a, String b){
+		this.a=a;	this.b=b;
+	}
+	public String toString() {
+		return b+" "+a;
+	}
+}
+
+public class ObjEx01 {
+	public static void main(String[] args) {
+		MyDog dog = new MyDog("멍멍이", "진돗개");
+		System.out.println(dog);
 	}
 }
---
+++ bm_workspace/MayJava/src/object/ObjEx02boxing.java
@@ -0,0 +1,60 @@
+package object;
+
+/*
+ * 1. boxing	기본 타입을 객체화 시킴
+ * 
+public class ObjEx02string {
+	public static void main(String[] args) {
+		Integer i = new Integer(10);	//정수 10을 객체화
+		//Integer i = 10;				
+		Character c = new Character('C');	
+		Boolean b = true;
+		
+		//기본 타입을 객체화 시킴 : boxing
+		//객체 -> 기본형으로 : unboxing
+		
+		Integer i1 = new Integer("10");
+	}
+}
+*/
+
+/*
+ * 2. unboxing	객체 -> 기본형으로
+ */
+public class ObjEx02boxing {
+	public static void main(String[] args) {
+		
+		Integer i = new Integer(10);	//boxing
+		int i1 = i.intValue();			//unboxing
+		
+		Character c = new Character('a');
+		char c1 = c.charValue();
+		
+		Boolean b = new Boolean(true);
+		boolean b1 = b.booleanValue();
+		
+		//문자열을 기본타입으로 변환!!
+		//parseInt : 기본타입으로 변경해주는것.
+		int i2 = Integer.parseInt("123");
+		boolean b2 = Boolean.parseBoolean("true");
+		double d2 = Double.parseDouble("3.14");
+		
+		int i3 = Integer.valueOf(123);
+		int i4 = Integer.valueOf("10")+Integer.valueOf("20");
+		
+		System.out.println(i3+i2);
+		//parse~~	: 리턴타입이 기본자료형 
+		//valueOf	: 리턴타입이 객체
+		
+		
+		String s = Integer.toString(123);
+		//정수형 객체 123을 문자열로 반환
+		
+		Integer i11 = 100;	//auto boxing
+					//Integer 클래스로 객체화시킴
+		int n1 = i11+200;	//auto unboxing
+		
+		
+		
+	}
+}
