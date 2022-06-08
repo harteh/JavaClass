f8d3048020fe16dc0145063b2a1386f788bf7c99
harteh <harteh31@gmail.com>
2022-06-08 12:46:09 +0900

    slider

--- bm_workspace/MayJava/src/awtFrame/Ex09Slider.java
+++ bm_workspace/MayJava/src/awtFrame/Ex09Slider.java
@@ -1,24 +1,45 @@
 package awtFrame;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class Ex09 extends JFrame {
+public class Ex09Slider extends JFrame {
 
 	JSlider j = new JSlider(100, 200, 150);
 	JLabel j1 = new JLabel("  ");
 	
-	Ex09(){
+	Ex09Slider(){
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
 		
 		j.setMajorTickSpacing(20);	//큰 눈금 간격
 		j.setPaintLabels(true);
 		j.setPaintTicks(true);
+		
+		j1.setHorizontalAlignment(JLabel.CENTER);
+		//문자열 위치를 중앙에 위치
+
+		j1.setOpaque(true);		//투명도 설정
+		j1.setBackground(Color.orange);
+		j1.setText(Integer.toString(j.getValue()));
+		//눈금 정수값을 문자열로 변경해서 프레임에 띄운다
+		
+		j.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// 눈금 변경 이벤트
+				j1.setText( Integer.toString(j.getValue()) );
+			}
+		});
+		
+		
 		
 		c.add(j);
 		c.add(j1);
@@ -29,7 +50,7 @@ public class Ex09 extends JFrame {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Ex09();
+		new Ex09Slider();
 	}
 
 }
