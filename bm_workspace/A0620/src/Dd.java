f97aaa170c4180b04a7fe1a652ce4bf0c2de60c4
harteh <harteh31@gmail.com>
2022-06-20 16:45:24 +0900

    JDBC connection 2

--- bm_workspace/A0620/src/Dd.java
+++ bm_workspace/A0620/src/Dd.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -6,6 +7,36 @@ import java.sql.SQLException;
 public class Dd {
 	public static void main(String[] args) throws SQLException {
 		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
+		String que = "insert into emp1(empno, ename, job) "
+				+"values(?,?,?)";
+		//? : 주기적으로 변경해서 입력할 값 (바인드변수)
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");		//1. 드라이버로드
+			con = DriverManager.getConnection(url, "BM", "bm0301");	//2. 연결객체생성
+			
+			pstmt = con.prepareStatement(que);						//3. 쿼리문 실행
+			
+			pstmt.setInt(1, 1234);
+			pstmt.setString(2, "JACK");
+			pstmt.setString(3, "ANALYST");
+			
+			int n = pstmt.executeUpdate();		//DB 갱신됨
+			
+			System.out.println(n > 0 ? "성공":"실패");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		/*
 		Connection conn = null;			//DB와 연결하는 인터페이스
 		PreparedStatement psmt = null;	//sql문 객체
 		ResultSet rs = null;			//sql에 대한 반환(쿼리 실행의 결과값 저장)
@@ -42,6 +73,7 @@ public class Dd {
 		rs.close();
 		psmt.close();
 		conn.close();
+		 */
 
 	}
 }
