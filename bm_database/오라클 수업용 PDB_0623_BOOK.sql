ca878d1c7a1cc6ffaeb6aef7b793f4fabf2c9f70
harteh <harteh31@gmail.com>
2022-06-21 20:02:22 +0900

    0623 sql Q

--- bm_database/오라클 수업용 PDB_0623_BOOK.sql
+++ bm_database/오라클 수업용 PDB_0623_BOOK.sql
@@ -4,7 +4,7 @@ CREATE TABLE BOOK(
   PUBLISHER VARCHAR2(30),
   PRICE NUMBER
 );
---DROP TABLE ORDERS;
+--DROP TABLE BOOK;
 select * from BOOK;
 INSERT INTO BOOK VALUES (1, '컴퓨터의 역사', '아이티', 7000);
 INSERT INTO BOOK VALUES (2, '컴퓨터는 무엇일까', '굿북', 13000);
@@ -34,21 +34,21 @@ select * from CUSTOMER;
 
 CREATE TABLE ORDERS(
   ORDERID CHAR(4) PRIMARY KEY,
-  CUSTID CHAR(4),
-  BOOKID CHAR(4),
+  CUSTID CHAR(4) CONSTRAINT ORDERS_CUSTID_FK REFERENCES CUSTOMER (CUSTID),
+  BOOKID CHAR(4) CONSTRAINT ORDERS_BOOKID_FK REFERENCES BOOK (BOOKID),
   SALEPRICE NUMBER,
   ORDERDATE DATE
 );
 
-INSERT INTO ORDERS VALUES (1, 1, 1, 6000, to_date('01-05-2022','dd-mm-yyyy'));
-INSERT INTO ORDERS VALUES (2, 1, 3, 21000, to_date('03-05-2022','dd-mm-yyyy'));
+INSERT INTO ORDERS VALUES (1, 1, 1, 7000, to_date('01-05-2022','dd-mm-yyyy'));
+INSERT INTO ORDERS VALUES (2, 1, 3, 23000, to_date('03-05-2022','dd-mm-yyyy'));
 INSERT INTO ORDERS VALUES (3, 2, 5, 8000, to_date('03-05-2022','dd-mm-yyyy'));
 INSERT INTO ORDERS VALUES (4, 3, 6, 6000, to_date('04-05-2022','dd-mm-yyyy'));
 INSERT INTO ORDERS VALUES (5, 4, 7, 20000, to_date('05-05-2022','dd-mm-yyyy'));
-INSERT INTO ORDERS VALUES (6, 1, 2, 12000, to_date('07-05-2022','dd-mm-yyyy'));
+INSERT INTO ORDERS VALUES (6, 1, 2, 13000, to_date('07-05-2022','dd-mm-yyyy'));
 INSERT INTO ORDERS VALUES (7, 4, 8, 13000, to_date('07-05-2022','dd-mm-yyyy'));
-INSERT INTO ORDERS VALUES (8, 3, 10, 12000, to_date('08-05-2022','dd-mm-yyyy'));
-INSERT INTO ORDERS VALUES (9, 2, 10, 7000, to_date('09-05-2022','dd-mm-yyyy'));
+INSERT INTO ORDERS VALUES (8, 3, 10, 13000, to_date('08-05-2022','dd-mm-yyyy'));
+INSERT INTO ORDERS VALUES (9, 2, 10, 13000, to_date('09-05-2022','dd-mm-yyyy'));
 INSERT INTO ORDERS VALUES (10, 3, 8, 13000, to_date('10-05-2022','dd-mm-yyyy'));
 --DROP TABLE ORDERS;
 select * from ORDERS;
@@ -116,25 +116,74 @@ SELECT SUM(SALEPRICE), AVG(SALEPRICE), MIN(SALEPRICE), MAX(SALEPRICE) FROM ORDER
 SELECT COUNT(*) FROM ORDERS;
 
 -- 20.	고객별로 주문한 도서의 총 수량과 총 판매액을 구해라.
-SELECT CUSTID, COUNT(*), SUM(SALEPRICE)
+SELECT CUSTID, COUNT(*) AS 총주문수량, SUM(SALEPRICE) AS 총주문금액
     FROM ORDERS
   GROUP BY CUSTID
   ORDER BY CUSTID;
 
 -- 21.	가격이 8000원 이상인 도서를 구매한 고객에 대하여 고객별 주문 도서의 총 수량을 구해라.
 -- 단,두 권 이상 구매한 고객만 구해라.
-
+SELECT CUSTID AS 회원ID, COUNT(*) AS 주문수량
+    FROM ORDERS
+  WHERE SALEPRICE >= 8000
+GROUP BY CUSTID;
 
 -- 22.	고객과 고객의 주문에 관한 데이터를 모두 보여라.
+SELECT  C.CUSTNAME AS 고객이름,
+        B.BOOKID AS 도서번호, B.BOOKNAME AS 도서이름,
+        O.ORDERID AS 주문번호, O.SALEPRICE AS 주문액, O.ORDERDATE AS 주문일
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID;
+
 -- 23.	고객과 고객의 주문에 관한 데이터를 고객번호 순으로 정렬하여 보여라.
+SELECT  C.CUSTID, C.CUSTNAME AS 고객이름, O.ORDERID AS 주문번호, 
+        B.BOOKID AS 도서번호, B.BOOKNAME AS 도서이름,
+        O.SALEPRICE AS 주문액, O.ORDERDATE AS 주문일
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID
+ORDER BY C.CUSTID;
+
 -- 24.	고객의 이름과 고객이 주문한 도서의 판매가격을 검색해라.
+SELECT C.CUSTNAME, B.PRICE
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID;
+  
 -- 25.	고객별로 주문한 모든 도서의 총 판매액을 구하고,고객별로 정렬해라.
+SELECT O.CUSTID, SUM(O.SALEPRICE)
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID
+GROUP BY O.CUSTID;
+  
 -- 26.	고객의 이름과 고객이 주문한 도서의 이름을 구해라.
+SELECT  C.CUSTNAME AS 고객이름, B.BOOKNAME AS 도서이름
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID;
+
 -- 27.	가격이 20000원인 도서를 주문한 고객의 이름과 도서의 이름을 구해라.
+SELECT  C.CUSTNAME AS 고객이름, B.BOOKNAME AS 도서이름
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID
+    AND B.PRICE = 20000;
+    
 -- 28.	도서를 구매하지 않은 고객을 포함하여 고객의 이름과 고객이 주문한 도서의 판매가격을 구해라.
+SELECT C.CUSTNAME, O.SALEPRICE
+    FROM CUSTOMER C LEFT OUTER JOIN ORDERS O ON (C.CUSTID = O.CUSTID)
+  ORDER BY C.CUSTNAME;
+
 -- 29.	가장 비싼 도서의 이름을 구해라.
+SELECT BOOKNAME FROM BOOK 
+  WHERE PRICE = (SELECT MAX(PRICE) FROM BOOK);
+
 -- 30.	도서를 구매한 적이 있는 고객의 이름을 검색해라.
+SELECT CUSTNAME FROM CUSTOMER
+  WHERE CUSTID IN (SELECT CUSTID FROM ORDERS);
+
 -- 31.	솔데스크에서 출판한 도서를 구매한 고객의 이름을 보여라.
+SELECT CUSTNAME 
+    FROM BOOK B, CUSTOMER C, ORDERS O
+  WHERE B.BOOKID = O.BOOKID AND C.CUSTID = O.CUSTID
+    AND B.PUBLISHER = '솔데스크';
+
 -- 32.	출판사별로 출판사의 평균 도서 가격보다 비싼 도서를 구해라.
 -- 33.	Book테이블에 새로운 도서 ‘공학 도서’를 삽입해라.공학 도서는 더샘에서 출간했으며 가격을 40000원이다.
 -- 34.	Book테이블에 새로운 도서 ‘공학 도서’를 삽입해라.공학 도서는 더샘에서 출간했으며 가격은 미정이다.
