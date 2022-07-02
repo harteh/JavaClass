254c53f37509caffc0063dbe4ba3531e331051fc
harteh <harteh31@gmail.com>
2022-07-02 20:39:14 +0900

    add project NFT admin page

---
+++ project_NFT/src/UserJoin/JoinTextField.java
@@ -0,0 +1,10 @@
+package UserJoin;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public interface JoinTextField {
+	
+	void setBoundTextField(int x, int y, int width, int height); 
+	
+}
